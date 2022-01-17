77f8b63d0e075efce6971478f85eaa2952dc90d7
fitareq <fitareq@gmail.com>
2022-01-17 23:35:53 +0600

    initial commit

---
+++ app/src/main/java/com/fitareq/contactbook/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fitareq.contactbook.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fitareq.contactbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
