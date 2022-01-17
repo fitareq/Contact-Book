008567f0749ce649d526c51450471b45134e72da
fitareq <fitareq@gmail.com>
2022-01-18 01:03:17 +0600

    initial commit

--- app/src/main/java/com/fitareq/contactbook/view/ContactFragment.java
+++ app/src/main/java/com/fitareq/contactbook/view/ContactFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,5 +32,8 @@ public class ContactFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+        navController.navigate(R.id.action_contactFragment_to_loginFragment);
     }
 }
